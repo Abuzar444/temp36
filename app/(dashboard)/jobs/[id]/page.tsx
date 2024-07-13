750f6dfcd22020640ca3785772e8d46ab4b349bf
= <=>
2024-07-13 13:25:25 +0500

    commit

--- app/(dashboard)/jobs/[id]/page.tsx
+++ app/(dashboard)/jobs/[id]/page.tsx
@@ -1,24 +1,28 @@
-import EditJobForm from '@/components/EditJobForm'
-import { getSingleJobAction } from '@/utils/actions'
+import EditJobForm from "@/components/EditJobForm";
+import { getSingleJobAction } from "@/utils/actions";
 
 import {
   dehydrate,
   HydrationBoundary,
   QueryClient,
-} from '@tanstack/react-query'
+} from "@tanstack/react-query";
 
 async function JobDetailPage({ params }: { params: { id: string } }) {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
-    queryKey: ['job', params.id],
-    queryFn: () => getSingleJobAction(params.id),
-  })
-
+  try {
+    await queryClient.prefetchQuery({
+      queryKey: ["job", params.id],
+      queryFn: () => getSingleJobAction(params.id),
+    });
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    // Handle unexpected error (e.g., display generic error message)
+  }
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <EditJobForm jobId={params.id} />
     </HydrationBoundary>
-  )
+  );
 }
-export default JobDetailPage
+export default JobDetailPage;
